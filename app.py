91f9fe1f4819b5c952141acff717f41274e41ddf
treggs1 <>
2023-11-14 12:42:16 +0000

    Add basic page structure and required css

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ mongo = PyMongo(app)
 def home_page():
     return render_template("home.html")
 
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=True)
+            port=int(os.environ.get("PORT")),
+            debug=True)
