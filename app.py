cad55d08548fc3d53f0084952444428bc56cc313
treggs1 <>
2023-11-16 06:34:54 +0000

    Create logout function

--- app.py
+++ app.py
@@ -61,6 +61,14 @@ def your_archive(username):
     return redirect(url_for("login"))
 
 
+@app.route("/logout")
+def logout():
+    # remove user form session cookies
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
