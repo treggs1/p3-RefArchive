0c166a747cdc1e561de71cc743b26a7c96ee46ff
treggs1 <>
2023-11-16 06:27:48 +0000

    Wire up your_archive and register pages to database

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -21,6 +22,43 @@ mongo = PyMongo(app)
 @app.route("/home_page")
 def home_page():
     return render_template("home.html")
+
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("your_archive", username=session["user"]))
+
+    return render_template("register.html")
+
+
+@app.route("/your_archive/<username>", methods=["GET", "POST"])
+def your_archive(username):
+    # grab the session user's username form db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+
+    if session['user']:
+        return render_template("your_archive.html", username=username)
+
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
