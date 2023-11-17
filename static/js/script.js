0a80859cc93d9d97a9b154646161e2e1fad5e051
Paul Treggiden <treggs1@gmail.com>
2023-11-17 02:49:05 +0000

    Add js and css for navbar

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,28 @@
+// Materialize Initialize
 $(document).ready(function(){
     $('select').formSelect();
   });
+
+// Nav JS this is a navbar I modified from another website I've been working on 
+// originally modified from https://jit.codes/r/Ofcq7DgOjpxUzbny1NhVR
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+const navSlide = () => {
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+      }
+    });
+
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
